28dea70d7272b0e59feadb3a934eb4b4c5aac1e4
abekorsequencebio <146199840+abekorsequencebio@users.noreply.github.com>
2025-06-09 17:25:00 -0230

    deals FE update

--- app/presenter/deal_presenter.rb
+++ app/presenter/deal_presenter.rb
@@ -1,7 +1,12 @@
-class DealPresenter < ApplicationPresenter
-	presents :deal
+class DealPresenter < BasePresenter
+  presents :deal
 
-	def timestamp
+  def time_range
+    return "" if deal.start_time.blank? || deal.end_time.blank?
+    "#{deal.start_time.strftime('%-l:%M')} - #{deal.end_time.strftime('%-l:%M %p')}"
+  end
+  
+  def timestamp
 		"#{deal.start_time.strftime("%l:%M %p")} – #{deal.end_time.strftime("%l:%M %p")}"
 	end
 end

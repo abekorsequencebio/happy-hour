17f05d5b81bb7a746d55f024b60ea9ccef3eabfc
abekorsequencebio <146199840+abekorsequencebio@users.noreply.github.com>
2025-06-10 11:33:51 -0230

    map controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,10 @@ class Restaurant < ApplicationRecord
   has_many :deals, dependent: :destroy
   validates :name, presence: true
 
+    # Geocoding setup
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  
   # Order by name by default
   default_scope { order(name: :asc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :restaurants
   resources :deals, only: [:new, :create, :edit, :update, :destroy]
 
+  get '/map', to: 'maps#show', as: 'map'
+
   get "up" => "rails/health#show", as: :rails_health_check
 end

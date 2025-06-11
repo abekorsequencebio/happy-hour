a75d6e12d59628081b9e01204a7cc126515c1d9e
abekorsequencebio <146199840+abekorsequencebio@users.noreply.github.com>
2025-06-11 14:39:17 -0230

    map backend

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "leaflet", to: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet.js"
+
